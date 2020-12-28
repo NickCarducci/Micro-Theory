ffc4c537ca4de59b5264d7ee090a322fd9a53b90
Nick <nmcarducci@gmail.com>
2020-12-28 15:00:47 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,168 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Micro-Theory</h1>
+      <h2>Platitudes</h2>
+      <br />
+      <span>
+        <b>
+          <i>We still live in colonialism </i>
+        </b>
+      </span>
+      <br />
+      <span>
+        <b>
+          <i>GDP is </i>
+        </b>
+        debt by a fabricated v1 from gdp/m1 where gdp from m2v2
+      </span>
+      <br />
+      <span>
+        <b>
+          <i>Rent is </i>
+        </b>
+        the prohibition of trade thru which to benefit from more than how much
+        they could possibly benefit in reality by owning the place, and not
+        trespassing at one time (>1m) (1/5)
+      </span>
+      <br />
+      <span>
+        <b>
+          <i>Debt, invoices & insurance brokering gift cards are </i>
+        </b>
+        to price-fix & monopolize the Demand curve & leverage other peoples'
+        autonomy in principal & interest, or the sellers' equity in principal &
+        other peoples' autonomy in interest
+      </span>
+      <br />
+      <span>
+        <b>
+          <i>Tax is </i>
+        </b>
+        theft by not being paid by those using services that cannot be a la
+        carte (voluntary)
+      </span>
+      <br />
+      <span>
+        <b>
+          <i>Slavery is </i>
+        </b>
+        an impossible contract, theft or indentured servitude that which is not
+        able to work out of without others' benefits at your disposal
+      </span>
+      <br />
+      <span>
+        or be able to pay but{" "}
+        <b>
+          not get paid due to preferences, or receive services due to impairment
+          or only under duress
+        </b>
+      </span>
+      <br />
+      <span>
+        or earning other people’s money (debt is the only bet that you can wager
+        someone else’s money)
+      </span>
+      <br />
+      <span>
+        Your debts are mine if I buy from you, and that money you promised is
+        not yours to promise.&nbsp;&nbsp;
+        <b>How can I get us out of debt when there is 82t:7t of it</b>
+      </span>
+      <br />
+      <h2>Strategy-Comparison to Macroeconomics</h2>
+      <br />
+      <span>
+        <b>
+          <i>Analysis by logic of markets-at-scale, </i>
+        </b>
+        not numbers by bets beyond escrow & involving others' marginal benefits
+        or utility expended as cost
+      </span>
+      <br />
+      <h2>Alternative Investing Models</h2>
+      <br />
+      <span>
+        <b>
+          <i>Customer borrowing, at a fixed- or max-profit price, </i>
+        </b>
+        although is not promising money that isn’t theirs like in interest or
+        time, it is price-fixing as a business can wait for customers to deliver
+        return, while an individual will have to charge their patrons to have
+        enough to buy food, health & shelter & the partner (really colonizer in
+        this situation)
+        {/**
+        (1) due to adding no competitor to raise quality of
+        life, just price, whose effect without benefit competitively is still
+        passing the burden on their customers,, (3) Prohibits
+        other people from trading the settled price not as a partnership to make
+        a product better at a settled price, but a customer max-profit royalty
+        if there is the ability to
+        foreclose it is not settled (2)
+        */}
+      </span>
+      <a
+        href="https://NationalSecurityCasino.com"
+        style={{
+          fontSize: "30px",
+          display: "flex",
+          position: "relative",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "76px",
+          backgroundColor: "white",
+          color: "black"
+        }}
+      >
+        NationalSecurityCasino.com
+      </a>
+      <a
+        href="https://SaverParty.xyz"
+        style={{
+          fontSize: "30px",
+          display: "flex",
+          position: "relative",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "76px",
+          backgroundColor: "#a64d79ff",
+          color: "rgb(230,230,230)"
+        }}
+      >
+        SaverParty.xyz
+      </a>
+      <a
+        href="https://Froth.app"
+        style={{
+          fontSize: "30px",
+          display: "flex",
+          position: "relative",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+          height: "76px",
+          backgroundColor: "#04060aff",
+          color: "#edeac1ef"
+        }}
+      >
+        Froth.app
+      </a>
+      <span>
+        <br />
+        <b>
+          <i>
+            Wealth is your assets relative to demand need, which may be as
+            valuable as toilet paper or colonialism{" "}
+          </i>
+        </b>
+        <br />
+        <br />
+      </span>
+    </div>
+  );
+}
